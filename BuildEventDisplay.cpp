23f44a1fe8c9fbbde764fae51fbfa09216a1cac9
Andreas Neiser <neiser@kph.uni-mainz.de>
2013-03-26 10:15:09 +0100

    Removed Eventdisplay

--- BuildEventDisplay.cpp
+++
@@ -1,7 +0,0 @@
-void BuildEventDisplay(){
-	gROOT->ProcessLine(".x BuildMapmtLibrary.cpp");
-	gROOT->ProcessLine(".x BuildTrbUnpacker.cpp");
-	gROOT->ProcessLine(".L TTrbDataTree.C+");
-	gROOT->ProcessLine(".L TTrbAnalysis.cpp+");
-	gROOT->ProcessLine(".L TTrbEventDisplay.cpp+");
-}
