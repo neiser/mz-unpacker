3149f42a1a0dd48253b62f9c72ac27c3aed7c633
Matthias Hoek <matthias.hoek@uni-mainz.de>
2013-07-12 23:39:22 +0200

    fixed event display building script
    building script did not build TTrbAnalysisBase object, is fixed now

--- BuildEventDisplay.cpp
+++ BuildEventDisplay.cpp
@@ -3,5 +3,8 @@ void BuildEventDisplay(){
 	gROOT->ProcessLine(".x BuildTrbUnpacker.cpp");
 	gROOT->ProcessLine(".L TTrbDataTree.C+");
 	gROOT->ProcessLine(".L TTrbAnalysis.cpp+");
+	cout << "Building TRB AnalysisBase ..." << endl;
+	gROOT->ProcessLine(".L TTrbAnalysisBase.cpp+");
+	cout << "Building TRB Event Display ..." << endl;
 	gROOT->ProcessLine(".L TTrbEventDisplay.cpp+");
 }
