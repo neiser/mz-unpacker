6119c40e8ec690e0f24b81b2c6cfeddbd07a13e1
Matthias Hoek <matthias.hoek@uni-mainz.de>
2015-02-10 17:34:08 +0100

    Build Macro added

---
+++ BuildFlashAnalysis.cpp
@@ -0,0 +1,41 @@
+void BuildFlashAnalysis(){
+	//gROOT->ProcessLine(".L TrbStructs.h+");
+	cout << "Building myUtilities ..." << endl;
+	gROOT->ProcessLine(".L myUtilities.cpp+");
+	////if(!TClass::GetClass("TTrbHit")->IsLoaded()){
+	//	cout << "Building TRB Hit class..." << endl;
+	//	gROOT->ProcessLine(".L TTrbHit.cpp+");
+	////}
+	////if(!TClass::GetClass("THldSubEvent")->IsLoaded()){
+	//	cout << "Building Subevent class ..." << endl;
+	//	gROOT->ProcessLine(".L THldSubevent.cpp+");
+	////}
+	////if(!TClass::GetClass("THldEvent")->IsLoaded()){
+	//	cout << "Building HLD Event class ..." << endl;
+	//	gROOT->ProcessLine(".L THldEvent.cpp+");
+	////}
+	////if(!TClass::GetClass("TTrbEventData")->IsLoaded()){
+	//	cout << "Building TRB Event Data class..." << endl;
+	//	gROOT->ProcessLine(".L TTrbEventData.cpp+");
+	////}
+	////if(!TClass::GetClass("TTrbUnpacker")->IsLoaded()) {
+	//	cout << "Building TRB Unpacker ..." << endl;
+	//	gROOT->ProcessLine(".L TTrbUnpacker.cpp+");
+	//}
+	//if(!TClass::GetClass("TTrbDataTree")->IsLoaded()){
+		cout << "Building TRB Data Tree ..." << endl;
+		gROOT->ProcessLine(".L TTrbDataTree.C+");
+	//}
+	//if(!TClass::GetClass("TTrbAnalysisBase")->IsLoaded()){
+		cout << "Building TRB AnalysisBase ..." << endl;
+		gROOT->ProcessLine(".L TTrbAnalysisBase.cpp+");
+	//}
+	//if(!TClass::GetClass("TDircAnalysisBase")->IsLoaded()){
+		cout << "Building DIRC Base Analysis ..." << endl;
+		gROOT->ProcessLine(".L TDircAnalysisBase.cpp+");
+	//}
+	//if(!TClass::GetClass("TDircAnalysisPostdoc")->IsLoaded()){
+		cout << "Building FLASH Analysis Analysis..." << endl;
+		gROOT->ProcessLine(".L TFlashAnalysis.cpp+");
+	//}
+}
